1072053b193d34b64b5cdae90a342dfa098e5af3
Nagendra1160 <50648646+Nagendra1160@users.noreply.github.com>
2020-06-20 13:12:24 +0530

    Add files via upload

---
+++ resume.js
@@ -0,0 +1,131 @@
+var root = document.getElementById("main");
+
+var div1 = document.createElement('div');
+div1.classList.add("card");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='profile.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Nagendra Thoram";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Software Developer";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="nagendrathoram@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="9838921988";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Hyderabad";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="index1.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
+
+
+var div1 = document.createElement('div');
+div1.classList.add("card1");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='profile.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Keerthi";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Software developer";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="keerthithram@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="9914323234";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Hyderabad";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="index1.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
+
+var div1 = document.createElement('div');
+div1.classList.add("card2");
+root.appendChild(div1);
+
+var image = document.createElement('img');
+image.src='profile.png';
+image.height=100;
+image.width=100;
+image.alt="picture";
+div1.appendChild(image);
+
+var heading =document.createElement('h2');
+heading.textContent="Suresh";
+div1.appendChild(heading);
+
+var heading =document.createElement('p');
+heading.textContent="Software developer";
+div1.appendChild(heading);
+
+div1.appendChild(document.createElement('hr'));
+
+var para = document.createElement('p');
+para.textContent="sureshthrma@gmail.com";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="9485728393";
+para.classList.add("p")
+div1.appendChild(para);
+
+var para = document.createElement('p');
+para.textContent="Hyderabad";
+para.classList.add("p")
+div1.appendChild(para);
+
+var a1  = document.createElement("a");
+a1.href="index1.html";
+a1.textContent="view";
+a1.classList.add('a');
+div1.appendChild(a1);
+
